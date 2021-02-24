39c60be134abcc18aa6f07724ddeebd1d437547a
KaitaiZhang <kaitaizh@usc.edu>
2021-02-24 10:22:47 -0800

    Update cpplib.h and README

--- src/lib/cpplib.h
+++ src/lib/cpplib.h
@@ -14,59 +14,11 @@ using namespace std;
  */
 class CPPLib {
  public:
-  //q2
+  //q3
   std::set<std::set<int>> twoSum(std::vector<int>& input, int sum);
 
-};
-
-//q3
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) { }
-};
-
-class SinglyLinkedList {
-public:
-  // default constructor
-  SinglyLinkedList();
-
-  //constructor creates a cyclic or acyclic linked list based on the value of i
-  SinglyLinkedList(vector<int> &input, int i);
-  
-  //destructor, cleans up
-  ~SinglyLinkedList();
-
-  bool empty();//checks if empty
-
-  int size(); //returns size
-
-  void push_back(int i);//inserts at the back
-
-  void push_front(int i);//inserts at the front
-
-  void insert_after(ListNode *p, int i);//inserts value i after p
-
-  void erase(ListNode *p);//erases node p
-
-  void pop_front();//remove the first item
-
-  void pop_back();//remove the last item
-
-  // back() and front() are optional now
-  // int back();//returns the value of last item
-
-  // int front();//returns the value of first item
-
-  ListNode* GetBackPointer();//returns the pointer to the last item
-
-  ListNode* GetIthPointer(int i);//returns pointer to ith element
-
-  void print();//prints the list: ex. Empty list: { }. List with Items: {1, 2, 3}
- 
-  void reverse(); // reverse the linked list in-place
-
-  ListNode *head_;//Pointer to the first element
+  //q6
+  bool​ ​CheckValidExpression​(​const​ ​string​&​ ​a​);
 
 };
 
